7768c6f6b0388b8c473dd99075bead593df18e6f
WaGaNaWa <m4a1carbin4@gmail.com>
2023-06-21 00:19:42 +0900

    Initial commit

---
+++ app/src/main/java/com/example/ttukttak_test/MainActivity.java
@@ -0,0 +1,99 @@
+package com.example.ttukttak_test;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.IntentSenderRequest;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.IntentSender;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.identity.BeginSignInRequest;
+import com.google.android.gms.auth.api.identity.BeginSignInResult;
+import com.google.android.gms.auth.api.identity.Identity;
+import com.google.android.gms.auth.api.identity.SignInClient;
+import com.google.android.gms.auth.api.identity.SignInCredential;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+public class MainActivity extends AppCompatActivity {
+
+    private SignInClient oneTapClient;
+    private BeginSignInRequest signUpRequest;
+    private static final int REQ_ONE_TAP = 2;  // Can be any integer unique to the Activity.
+
+    private boolean showOneTapUI = true;
+
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.test_login);
+        oneTapClient = Identity.getSignInClient(this);
+        signUpRequest = BeginSignInRequest.builder()
+                .setGoogleIdTokenRequestOptions(BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
+                        .setSupported(true)
+                        // Your server's client ID, not your Android client ID.
+                        .setServerClientId(getString(R.string.web_client_id))
+                        // Show all accounts on the device.
+                        .setFilterByAuthorizedAccounts(false)
+                        .build())
+                .build();
+
+        ActivityResultLauncher<IntentSenderRequest> activityResultLauncher =
+                registerForActivityResult(new ActivityResultContracts.StartIntentSenderForResult(), new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        try {
+                            SignInCredential credential = oneTapClient.getSignInCredentialFromIntent(result.getData());
+                            String idToken = credential.getGoogleIdToken();
+                            if (idToken !=  null) {
+                                Log.d("ONE-TAP", "Got ID token.");
+                                Log.d("ONE-TAP",idToken);
+                                String email = credential.getId();
+
+                                Toast.makeText(MainActivity.this, "email : " + email, Toast.LENGTH_SHORT).show();
+                                Log.d("ONE-TAP", "Got G ID :" + email);
+
+
+                            }
+                        } catch (ApiException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                oneTapClient.beginSignIn(signUpRequest)
+                        .addOnSuccessListener(MainActivity.this, new OnSuccessListener<BeginSignInResult>() {
+                            @Override
+                            public void onSuccess(BeginSignInResult result) {
+                                IntentSenderRequest intentSenderRequest =
+                                        new IntentSenderRequest.Builder(result.getPendingIntent().getIntentSender()).build();
+                                activityResultLauncher.launch(intentSenderRequest);
+                            }
+                        })
+                        .addOnFailureListener(MainActivity.this, new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                // No Google Accounts found. Just continue presenting the signed-out UI.
+                                Log.d("ONE-TAP", e.getLocalizedMessage());
+                            }
+                        });
+            }
+        });
+    }
+}
